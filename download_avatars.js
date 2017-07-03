e97683ada9d7dd635f281e5a08a8cd67fcab6369
Alexandra Levy <alexandrasophielevy@gmail.com>
2017-07-03 18:57:52 +0000

    print each avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,28 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+var GITHUB_USER = 'alexandrasophielevy'
+var GITHUB_TOKEN = '6238442fe70fb4afdebb862ccf75e464416ffaa2'
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var URL = 'https://'
+  URL += GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
+  var options = {
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    },
+    url: URL
+  }
+  request(options, function(err, response, body) {
+    var jsonBody = JSON.parse(body)
+    cb(err, jsonBody)
+  })
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+  for(var i = 0; i < result.length; i++) {
+    console.log(result[i].avatar_url)
+  }
+});
+
