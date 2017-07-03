0138e73131b79b91de9f8340e7b6ac229bba89dc
Alexandra Levy <alexandrasophielevy@gmail.com>
2017-07-03 19:12:56 +0000

    implement downloadImageByURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = 'alexandrasophielevy'
 var GITHUB_TOKEN = '6238442fe70fb4afdebb862ccf75e464416ffaa2'
@@ -26,3 +27,16 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   }
 });
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function(err) {
+    throw err;
+  })
+  .on('response', function(response) {
+    console.log('Response Status Message: ', response.statusMessage)
+    console.log('Reponse Content Type: ', response.headers['content-type'])
+  })
+  .pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
